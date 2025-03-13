ebc313dec6c1bb6c9bb44e213b6b1d1451a09ee5
RobAntunes <robm.antunes@gmail.com>
2025-03-13 10:12:59 +0100

    add padding top to contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-      <div className="bg-white py-8 sm:pt-24">
+      <div className="bg-white py-8 sm:pt-40">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
             <div className="grid grid-cols-1 gap-10 py-16 lg:grid-cols-3">
